08cb49811a80d43178858583d9a6dde9ad265975
victor yeswa <victoryeswa01@gmail.com>
2023-03-13 18:22:33 +0300

    Update matplotlib tutorials.py

--- matplotlib tutorials.py
+++ matplotlib tutorials.py
@@ -6,7 +6,14 @@ import pandas as pd
 #Basic graph
 x = [1,2,3]
 y = [2,4,6]
-plt.plot(x,y, label='2x', color ='yellow',linewidth=2, marker='_',markersize=10)
+
+#resize your graph
+plt.figure(figsize=(5,3), dpi=100)
+plt.plot(x,y, label='2x', color ='yellow',linewidth=2, marker='.',linestyle='--',markersize=10, markeredgecolor='blue')
+# line number two
+x2 =np.arange(0,4.5,0.5)
+plt.plot(x2[:6], x2[:6]**2, 'r', label='x^2')
+plt.plot(x2[5:],x2[5:]**2, 'r--')
 plt.legend()
 #title
 plt.xlabel('x-label')
@@ -15,4 +22,14 @@ plt.title('My first Graph', fontdict={'fontname':'', 'fontsize':20})
 plt.style.use('Solarize_Light2')
 plt.xticks([0,1,2,3,4])
 plt.yticks([0,2,4,6,7,10])
+
+#to save your plot
+plt.savefig('mygrapgh.png', dpi=100)
+plt.show()
+
+#using a short hand notation
+# fmt = '[color][marker][line]
+x = [2,4,5,7]
+y = [3,6,9,12]
+plt.plot(x,y, 'r.-', label='2x')
 plt.show()
