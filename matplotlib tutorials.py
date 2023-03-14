bd7b5fbf30dd6a999e41e723d3658fb936a21e11
victor yeswa <victoryeswa01@gmail.com>
2023-03-14 10:27:13 +0300

    Update matplotlib tutorials.py

--- matplotlib tutorials.py
+++ matplotlib tutorials.py
@@ -15,9 +15,11 @@ x2 =np.arange(0,4.5,0.5)
 plt.plot(x2[:6], x2[:6]**2, 'r', label='x^2')
 plt.plot(x2[5:],x2[5:]**2, 'r--')
 plt.legend()
-#title
+
+#plot labels
 plt.xlabel('x-label')
 plt.ylabel('y-label')
+#title
 plt.title('My first Graph', fontdict={'fontname':'', 'fontsize':20})
 plt.style.use('Solarize_Light2')
 plt.xticks([0,1,2,3,4])
@@ -33,3 +35,4 @@ x = [2,4,5,7]
 y = [3,6,9,12]
 plt.plot(x,y, 'r.-', label='2x')
 plt.show()
+#bar charts
