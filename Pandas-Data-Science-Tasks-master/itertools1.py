67acd05c079d4fe648686257399d5a1da5bb71f0
victor yeswa <victoryeswa01@gmail.com>
2023-03-30 07:48:26 +0300

    Create itertools1.py

---
+++ Pandas-Data-Science-Tasks-master/itertools1.py
@@ -0,0 +1,19 @@
+#itertools: product, permutations, accumilate, groupby, and infinite iterators
+
+from itertools import product
+
+a = [1, 2]
+
+b = [3, 4]
+
+prod = product(a, b)
+
+print(list(prod))
+
+#we can define the number of repetitions
+
+a = [1, 2]
+b = [3]
+prod1 = product (a, b, repeat=2)
+
+print(list(prod1))
